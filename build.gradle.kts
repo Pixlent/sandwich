de24fec4cd36cfabc5d7d215ed0484ddf01c9434
vearen <vearen@outlook.com>
2025-02-21 18:58:13 +0100

    chore: bump minestom

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("net.minestom:minestom-snapshots:3bdb950310")
+    implementation("net.minestom:minestom-snapshots:1_21_4-b7c38fd36b")
     implementation("de.articdive:jnoise-pipeline:4.1.0")
     implementation("com.github.CoolLoong:FastNoise2Bindings-Java:0.0.1")
     implementation("org.slf4j:slf4j-simple:2.0.16")
